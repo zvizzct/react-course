368ec2a1b372b958010ea2e4679d05c17ff03c0d
zvizzct <victor.puerta01@estudiant.upf.edu>
2023-04-12 12:07:23 +0200

    hooks generales

---
+++ 05-hook-app/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { HooksApp } from './HooksApp';
+import { CounterApp } from './01-useState/CounterApp';
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+import { SimpleForm } from './02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { FocusScreen } from './04-useRef/FocusScreen';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  <FocusScreen />
+  // </React.StrictMode>
+);
